9c77bd7040b0f12da46cc718a2cb78a20437fac7
PreetJawla <preetjawla6@gmail.com>
2024-06-28 23:47:17 +0530

    Update DeadlyToken.sol

--- DeadlyToken.sol
+++ DeadlyToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20("Deadly Token", "DT"), ERC20Burnable, Ownable(msg.sender) {
+contract DeadlyToken is ERC20("Deadly Token", "DT"), ERC20Burnable, Ownable(msg.sender) {
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount*10**2);
