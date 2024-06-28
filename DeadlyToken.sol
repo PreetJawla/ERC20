7cf4806ee5c5bc3668a5001b61250bc102cb6052
PreetJawla <preetjawla6@gmail.com>
2024-06-28 23:43:43 +0530

    Create DeadlyToken.sol

---
+++ DeadlyToken.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20("Deadly Token", "DT"), ERC20Burnable, Ownable(msg.sender) {
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount*10**2);
+    }
+
+    function burn(uint256 amount) public override {
+        _burn(msg.sender, amount);
+    }
+
+    function tranfer(address to, uint256 amount) public {
+        _transfer(msg.sender, to, amount);
+    }
+
+}
